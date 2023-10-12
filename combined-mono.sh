ebea5ad575aaec62fec7b0a538647d63a1b75b12
JP Rosevear <jp@nubinary.com>
2023-10-12 14:11:33 -0400

    Allow for configuration

--- combined-mono.sh
+++ combined-mono.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-GIT_REPO_BASE="boilerplate_"
+GIT_REPO_BASE=${1:-boilerplate_}
+GIT_REPO_ORG=${2:-nubinary}
 CLIENT_REPO_NAME="${GIT_REPO_BASE}client"
 SERVER_REPO_NAME="${GIT_REPO_BASE}server"
 MONO_REPO_NAME="${GIT_REPO_BASE}mono"
 
-rm -rf ./mono-construction
+#rm -rf ./mono-construction
 
 mkdir -p "mono-construction/${MONO_REPO_NAME}"
 cd  mono-construction
 
-git clone "git@github.com:nubinary/${CLIENT_REPO_NAME}.git"
+git clone "git@github.com:${GIT_REPO_ORG}/${CLIENT_REPO_NAME}.git"
 cd $CLIENT_REPO_NAME
 git-filter-repo --to-subdirectory-filter client
 
 cd ..
 
-git clone "git@github.com:nubinary/${SERVER_REPO_NAME}.git"
+git clone "git@github.com:${GIT_REPO_ORG}/${SERVER_REPO_NAME}.git"
 cd $SERVER_REPO_NAME
 git-filter-repo --to-subdirectory-filter server
 
@@ -29,7 +30,7 @@ git merge $CLIENT_REPO_NAME/main --allow-unrelated-histories
 
 git remote add $SERVER_REPO_NAME ../$SERVER_REPO_NAME
 git fetch $SERVER_REPO_NAME
-git merge $SERVER_REPO_NAME/main --allow-unrelated-histories
+git merge $SERVER_REPO_NAME/main --allow-unrelated-histories --no-edit
 
 
 
